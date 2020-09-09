e9255d48e204ae40199dda6a8afe5a7023650f1a
hoanha <hoanbk2.16@gmail.com>
2020-09-09 16:50:01 +0700

    change view

--- DemoMaps/Scripts/customs/maps.js
+++ DemoMaps/Scripts/customs/maps.js
@@ -16,7 +16,7 @@ function createChart(seriesOptions) {
         yAxis: {
             labels: {
                 formatter: function () {
-                     return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                    return this.value;
                 }
             },
             plotLines: [{
@@ -28,17 +28,24 @@ function createChart(seriesOptions) {
 
         plotOptions: {
             series: {
-                compare: 'percent',
+               
                 showInNavigator: true
             }
         },
 
         tooltip: {
-            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+            pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b><br/>',
             valueDecimals: 2,
             split: true
         },
-
+        title: {
+            text: "Biểu đồ giá vàng SJC 1 năm - <small>từ ngày 09/09/2019 đến ngày 09/09/2020</small> (đơn vị triệu đồng / lượng)"
+        },
+        subtitle: {
+            text: "<br><strong style='color:#008000'>–––</strong> VN <strong style='color:#ff0000; padding-left:20px'>–––</strong> TG",
+            useHTML: true
+        },
+        colors: ["green", "red", "#90ed7d", "#f7a35c", "#8085e9", "#f15c80", "#e4d354", "#2b908f", "#f45b5b", "#91e8e1"],
         series: seriesOptions
     });
 }
